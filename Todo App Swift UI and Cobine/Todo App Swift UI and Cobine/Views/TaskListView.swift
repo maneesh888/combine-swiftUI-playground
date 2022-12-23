f344fadd31504fbf0f9214e1d419c4c03f2d9ea3
Maneesh <maneesh888@gmail.com>
2022-12-23 20:14:27 +0400

    View title and button aligned properly

--- Todo App Swift UI and Cobine/Todo App Swift UI and Cobine/Views/TaskListView.swift
+++ Todo App Swift UI and Cobine/Todo App Swift UI and Cobine/Views/TaskListView.swift
@@ -18,19 +18,19 @@ struct TaskListView: View {
                         TaskCell(task: task)
                     }
                 }
-            }
+                
+                Button(action: {}) {
+                    HStack {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                        Text("New Task")
+                    }
+                }
+                .padding()
+                .accentColor(Color(UIColor.systemRed))
+            }.navigationBarTitle("Tasks")
         }
-        Button(action: {}) {
-            HStack {
-                Image(systemName: "plus.circle.fill")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                Text("New Task")
-            }
-        }
-        .padding()
-        .accentColor(Color(UIColor.systemRed))
-        .navigationBarTitle("Tasks")
     }
 }
 
